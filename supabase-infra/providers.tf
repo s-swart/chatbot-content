10bb3eea8e5646a6176f27e57b5d08e59624b8da
s-swart <73905973+s-swart@users.noreply.github.com>
2025-05-17 15:42:25 -0400

    deleting tf, going with supabase cli

--- supabase-infra/providers.tf
+++
@@ -1,8 +0,0 @@
-provider "postgresql" {
-  host            = var.db_host
-  port            = 5432
-  database        = "postgres"
-  username        = "postgres"
-  password        = var.db_password
-  sslmode         = "require"
-}
